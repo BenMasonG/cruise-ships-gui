6c0efe2eda372caa99a0cfe6141a1811ad90fe4b
Benjamin Mason-Gallagher <BenMasonG@protonmail.com>
2022-05-05 16:30:24 +0100

    Port test refactored to utilise beforeEach function and remove repeated declearation of variables in each test.

--- __tests__/port.test.js
+++ __tests__/port.test.js
@@ -1,44 +1,47 @@
 const Port = require('../src/port');
 
-describe('Port', () => {
+describe('Port', () => { 
+  describe ('Port and it\'s methods', () => {
+    let dover;
+    let porto;
+    let ship;
+    let ship2;
+    let ship3;
+
+    beforeEach(() => {
+      dover = new Port ('Dover');
+      porto = new Port ('Porto');
+      ship = {};
+      ship2 = {};
+      ship3 = {};
+   });
+
     it('can be instantiated', () => {
      expect(new Port()).toBeInstanceOf(Object);
     });
-  });
 
-  describe('Port properties', () => {
     it('tests that I can create a new Port and give it a name', () => {
-      const dover = new Port ('Dover');
-      const porto = new Port ('Porto');
-  
+      
       expect(dover.name).toBe('Dover');
       expect(porto.name).toBe('Porto');
     });
-  });
+  
 
-  describe('Port method addShip', () => {
+  
     it('can add a ship', () => {
-      const port = new Port ('Dover');
-      const ship = {};
+      dover.addShip(ship);
 
-      port.addShip(ship);
-
-     expect(port.ships).toContain(ship);
+      expect(dover.ships).toContain(ship);
     });
-  });
-
-  describe('Port method removeShip', () => {
+  
     it('can remove a ship', () => {
-      const port = new Port ('Dover');
-      const ship = {};
-      const ship2 = {};
-      const ship3 = {};
-      port.addShip(ship);
-      port.addShip(ship2);
+      dover.addShip(ship);
+      dover.addShip(ship2);
 
-      port.removeShip(ship)
+      dover.removeShip(ship)
 
-     expect(port.ships).toEqual([ship2]);
-     expect(()=> port.removeShip(ship3)).toThrowError('The named ship is not currently at this port.');
+     expect(dover.ships).toEqual([ship2]);
+     expect(()=> dover.removeShip(ship3)).toThrowError('The named ship is not currently at this port.');
     });
   });
+});
