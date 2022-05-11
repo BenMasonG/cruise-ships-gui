a884f9c12142e1df237a7a39723c58f8d07cb7b4
Benjamin Mason-Gallagher <BenMasonG@protonmail.com>
2022-05-11 15:08:51 +0100

    A button was added to the HTML doc then styled using CSS. A setSail funciton was added to the controller. This can be called via an event listener on the button. The ship can now sail to all ports in the Itinerary.

--- src/controller.js
+++ src/controller.js
@@ -1,6 +1,12 @@
 (function exportController() {
-function Controller () {
+function Controller (ship) {
+    this.ship = ship
+
     this.initialiseSea();
+
+    document.querySelector('#sailbutton').addEventListener('click', () => {
+        this.setSail();
+    });
 };
 
   Controller.prototype.initialiseSea = function initialiseSea () {
@@ -30,14 +36,41 @@ function Controller () {
           })
       };
 
-  Controller.prototype.renderShip = function renderShip (ship) {
+  Controller.prototype.renderShip = function renderShip () {
+    const ship = this.ship;
+    
     const shipPortIndex = ship.itinerary.ports.indexOf(ship.currentPort);
     const portsElement = document.querySelector(`[data-port-index='${shipPortIndex}']`);
+
     const shipElement = document.querySelector('#ship');
     shipElement.style.top = `${portsElement.offsetTop + 32}px`;
     shipElement.style.left = `${portsElement.offsetLeft - 32}px`;
   }
     
+  Controller.prototype.setSail = function setSail () {
+      const ship = this.ship;
+
+      const currentPortIndex = ship.itinerary.ports.indexOf(ship.currentPort);
+      const nextPortIndex = currentPortIndex + 1;
+      const nextPortElement = document.querySelector(`[data-port-index='${nextPortIndex}']`);
+
+      if (!nextPortElement) {
+        return alert('End of the line!');
+    };
+
+      const shipElement = document.querySelector('#ship');
+      const sailInterval = setInterval(() => {
+          const shipLeft = parseInt(shipElement.style.left, 10);
+          if (shipLeft === (nextPortElement.offsetLeft - 32)) {
+              ship.setSail();
+              ship.dock();
+              clearInterval(sailInterval);
+          }
+
+          shipElement.style.left = `${shipLeft + 1}px`;
+      }, 20)
+  };
+
   if (typeof module !== 'undefined' && module.exports) {
     module.exports = Controller;
  } else {
